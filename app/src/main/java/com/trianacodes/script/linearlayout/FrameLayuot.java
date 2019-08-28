c25acf7d56b38c6033ed4b7d09c87bb94e5853f6
Worome <41673409+Worome@users.noreply.github.com>
2019-08-28 10:27:38 +0200

    LinearLayout, TableLayout y FrameLayout

---
+++ app/src/main/java/com/trianacodes/script/linearlayout/FrameLayuot.java
@@ -0,0 +1,13 @@
+package com.trianacodes.script.linearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameLayuot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layuot);
+    }
+}
